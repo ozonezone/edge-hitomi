3e6a6f0042a1900208b23c1d8d1ab4a1f8899300
jiyoo dojun <63794592+KingMedieval@users.noreply.github.com>
2024-12-11 16:29:46 -0800

    WIP

--- src/new/getGallery.ts
+++ src/new/getGallery.ts
@@ -1,4 +1,4 @@
-import {Gallery, Image} from "../type";
+import {Gallery, Image, Tag} from "../type";
 import {edgeFetch} from "./edgeFetch";
 import {contentTypes, JSONGallery} from "./newType";
 import {HitomiError} from "./HitomiError";
@@ -7,9 +7,8 @@ import {ERROR_CODE} from "./namedConstants";
 export async function getGallery(id: number, userHeaders?: Headers): Promise<Gallery> {
 	const response: JSONGallery = await edgeFetch('ltn.', `/galleries/${id}.js`, userHeaders)
 		.then((res: Uint8Array<ArrayBuffer>) =>
-			(new TextDecoder().decode(res)).replace(/^var +\w+ *= */, '') as unknown as JSONGallery);
-
-	const gallery: Gallery = {
+			JSON.parse((new TextDecoder().decode(res)).replace(/^var +\w+ *= */, '')) as unknown as JSONGallery);
+	const gallery = {
 		id: Number.parseInt(response.id as string),
 		title: {
 			display: response.title,
@@ -24,11 +23,11 @@ export async function getGallery(id: number, userHeaders?: Headers): Promise<Gal
 		groups: response.groups?.map(({ group }) => group) || [],
 		series: response.parodys?.map(({ parody }) => parody) || [],
 		characters: response.characters?.map(({ character }) => character) || [],
-		tags: response.tags.map(({ tag, male, female }) => {
-			if (!male && !female) return { type: 'tag', name: tag };
+		tags: response.tags?.map(({ tag, male, female }) => {
+			if (!male && !female) return { type: 'tag', name: tag } as Tag;
 			// see /galleries/111.js for numeric literals, new galleries have string literals
-			else if (male == '' && (female == 1 || female == '1')) return { type: 'female', name: tag };
-			else if (female == '' && (male == 1 || male == '1')) return {type: 'male', name: tag};
+			else if (male == '' && (female == 1 || female == '1')) return { type: 'female', name: tag } as Tag;
+			else if (female == '' && (male == 1 || male == '1')) return {type: 'male', name: tag} as Tag;
 			else throw new HitomiError(ERROR_CODE.INVALID_TAG, tag);
 		}),
 		files: response.files?.map(({ hash, name, hasavif, haswebp, hasjxl, width, height }, index) => ({
